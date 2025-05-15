5406a41025c8271c55ed52d06ecb8b09a0df2fbb
YuHlushchenko <yuhlushchenko@gmail.com>
2025-05-15 10:12:26 +0300

    init the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fe-test-250515
+
 This repository contains a front-end coding exercise completed as part of a technical assessment process
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return <Typography>lol</Typography>;
+}
+
+export default App;
